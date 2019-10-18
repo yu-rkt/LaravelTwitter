8430974c4ab74d861cc7b1c5ae66aa0ee5eee79a
watanabeyu <y.watanabe.rkt@gmail.com>
2019-10-18 18:45:33 +0900

    フォロー機能を実装しました。

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,19 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// 現在認証されているユーザー(自分)を取得するときにAuth::user();を使うためにAuthをuseする。
+use Illuminate\Support\Facades\Auth;
 
 use App\Follow;
 use App\Tweet;
-use Appzz\User;
+use App\User;
 
 class UsersController extends Controller
 {
+    // Userモデルにログイン済みのユーザー(自分)以外の全てのユーザーを取得するgetAllUsersメソッドを作成し、
+    // その関数をUsersControllerで呼び出すときに引数として認証ユーザーを設定してあげる。
+    // そしてgetAllUsersメソッドで取得した値を$all_users変数に格納してあげて、viewに渡す。
+
+    // ユーザー一覧を取得
     public function index(User $user)
     {
-      $my_user = $user->getAllUsers(auth()->user()->id);
+      $all_users = $user->getAllUsers(Auth::id());
 
-      return view('list', [
-        'my_user' => $my_user
+      return view('user.index', [
+        'all_users' => $all_users
       ]);
+    }
+
+    // フォローする関数
+    // フォローしているかを判断し、フォローしていなければフォローする。
+    public function follow(User $user)
+    {
+      $follower = Auth::users();
+
+      $is_following = $follower->isFollowing($user->id);
+      // $is_followingに該当する値がなかったら
+      if(!$is_following) {
+        $follower->follow($user->id);
+
+        return back();
+      }
+    }
+
+    // フォロー解除する
+    public function unfollow(User $user)
+    {
+      $follower = Auth::users();
+
+      $is_following = $follower->isFollowing($user->id);
+
+      // $is_followingに該当する値があったら
+      if($is_following) {
+        $follower->unfollow($user->id);
+
+        return back();
+      }
     }
 }
--- app/User.php
+++ app/User.php
@@ -37,10 +37,17 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    // フォローする側とフォローされる側は多対多の関係のため、多対多のリレーションを設定する。
+    // belongsToMany('', '', '', '');
+    // 第一引数・・関連するモデル名。ここではUserモデル
+    // 第二引数・・中間テーブル名
+    // 第三、第四引数・・外部キー
     public function follows()
     {
-      return $this->hasMany('App\Follow');
+      return $this->belongsToMany(self::class, 'follows', 'following_id'. 'followed_id');
     }
+
+
 
     // $follow_ids配列に自分とフォローしているユーザーを格納し、その中からtweetsテーブルのuser_idカラムに該当するものを取り出し
     // orderByで降順に並べる
@@ -50,9 +57,30 @@ class User extends Authenticatable
       return $this->whereIn('user_id', $follow_ids)->orderBy('created_at', 'desc')->paginate(10);
     }
 
-    // ログインしているユーザー以外の全てのうユーザーを10人取得する
+    // ログインしているユーザー以外の全てのユーザーを10人取得する
     public function getAllUsers(Int $user_id)
     {
-        return $this->Where('id', '<>', $user_id)->paginate(10);
+      return $this->where('id', '<>', $user_id)->paginate(10);
     }
+
+    // フォローしているかを判断する。
+    // followed_idの中から引数の$user_idを探し,複数あるので最初のidのみ取得する
+    public function isFollowing(Int $user_id)
+    {
+      return $this->follow()->where('followed_id', $user_id)->first(['id']);
+    }
+
+    // フォローする。
+    // followメソッドにアクセス→引数で受け取った$user_idをattachする
+    public function follow(Int $user_id)
+    {
+      $this->follow()->attach($user_id);
+    }
+
+    // フォロー解除する。
+    public function unfollow(Int $user_id)
+    {
+      $this->follow()->detach($user_id);
+    }
+
 }
